2d00bb66796bb2c3754f09a46656c3b0040a1103
ganeodolu <ganeodolu@gmail.com>
2022-02-19 19:28:02 +0900

    build: 환경설정
    webpack, eslint, prettier, babel

---
+++ client/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: path.resolve(__dirname, "src/index.js"),
+	output: {
+		path: path.resolve(__dirname, "output"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html",
+		}),
+	],
+};
