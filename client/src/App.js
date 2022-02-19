474922edf22161f2bfdbe8eca63da4ce9379b8a1
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2022-02-19 22:30:20 +0900

    init: 프로젝트 초기 환경설정

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  const a = 123;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
