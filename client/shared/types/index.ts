943cbb3bc3d2b2c99d077709137660ec2998feb8
g1moon <gmoon.cho@gmail.com>
2022-02-22 21:10:29 +0900

    feat: shared type 추가 - feat: Redux 관련 타입 추가 (BaseRequestReducerState, RootState, AppDispatch) - feat: Request 관련 타입 추가 (RequestMethod, RequestInfo, RequestInfoWithParams, Request, REQUEST_TYPE, ReturnedRequestInfo) - feat: COLOR 상수 관련 타입 추가 (Color)

---
+++ client/shared/types/index.ts
@@ -0,0 +1,32 @@
+import { store } from '../../store';
+import { COLOR } from '../constants/css';
+import { REQUEST } from '../constants/url';
+
+export type Color = typeof COLOR[keyof typeof COLOR];
+
+export type RequestMethod = 'get' | 'post' | 'delete' | 'put';
+export type RequestInfo = { method: RequestMethod; url: string };
+export type RequestInfoWithParams = (id?: string) => RequestInfo;
+export type Request = {
+  [key: string]: RequestInfoWithParams;
+};
+export type REQUEST_TYPE = typeof REQUEST[keyof typeof REQUEST];
+export type ReturnedRequestInfo = ReturnType<REQUEST_TYPE>;
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+}
+
+// redux
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export interface BaseRequestReducerState<T> {
+  value: T;
+  isLoading: boolean;
+  hasError: boolean;
+  error: Error | null;
+}
