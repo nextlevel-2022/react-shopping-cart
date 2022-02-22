5be9e29ba256351d2d9081f3062221ab33898a9b
g1moon <gmoon.cho@gmail.com>
2022-02-22 21:32:57 +0900

    feat: 무한 스크롤 기능 적용하는 InfiniteScroll 컴포넌트 추가 - feat: 브라우저 가장 하단에 도착했는지 확인하는 returnIsBottom 함수 추가 - feat: 스크롤 시 하단인지 확인하고, 특정 작업(props로 받아온)을 하는 기능 추가 - feat: scroll 이벤트 발생시 디바운스 적용된 함수 실행하도록 하는 기능 추가

---
+++ client/components/InfiniteScroll/InfiniteScroll.tsx
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/named
+import { debounce } from 'lodash';
+import { useEffect } from 'react';
+
+interface Props {
+  workToDoWhenArrivedBottom: () => void;
+  msToDelay?: number;
+  isLastPage: boolean;
+}
+
+const InfiniteScroll = ({ workToDoWhenArrivedBottom, msToDelay = 100 }: Props) => {
+  const returnIsBottom = (): boolean => {
+    const { innerHeight } = window;
+    const {
+      body: { scrollHeight },
+      documentElement: { scrollTop },
+    } = document;
+
+    return Math.round(scrollTop + innerHeight) >= scrollHeight;
+  };
+
+  const debouncedOnScroll = debounce(() => {
+    if (returnIsBottom()) {
+      workToDoWhenArrivedBottom();
+    }
+  }, msToDelay);
+
+  useEffect(() => {
+    window.addEventListener('scroll', debouncedOnScroll);
+  }, [debouncedOnScroll]);
+
+  return <></>;
+};
+
+export default InfiniteScroll;
