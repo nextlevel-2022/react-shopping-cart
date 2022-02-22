95efc06910f1a0ebefe92d6a320b09c166a007eb
g1moon <gmoon.cho@gmail.com>
2022-02-22 21:28:31 +0900

    feat: 단일 상품을 보여주는 ProductItem 컴포넌트 추가 - feat: ProductList 컴포넌트로 부터 product 정보를 받아와 보여주는 기능 추가   -feat: 제품 이미지, 이름, 가격을 보여주는 기능 추가

---
+++ client/components/ProductList/ProductItem.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import { URL } from '../../shared/constants/url';
+import { Product } from '../../shared/types';
+
+interface Props {
+  product: Product;
+}
+
+const ProductItem = ({ product }: Props) => {
+  const { id, name, price, imageUrl } = product;
+
+  return (
+    <Container>
+      <Link href={URL.PRODUCT_DETAIL(id)} passHref>
+        <Image src={imageUrl} alt={name} />
+      </Link>
+      <InformationContainer>
+        <Name>{name}</Name>
+        <Price>{price} 원</Price>
+      </InformationContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+const Image = styled.img`
+  width: 17.625rem;
+  height: 17.625rem;
+  cursor: pointer;
+`;
+
+const InformationContainer = styled.div`
+  margin: 0.5rem 0 0.5rem 0.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const Name = styled.span`
+  font-size: 1rem;
+`;
+const Price = styled.span``;
+
+export default ProductItem;
