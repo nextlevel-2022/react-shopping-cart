c5df4d6159ddabae5694d8933db1c66774be7a64
g1moon <gmoon.cho@gmail.com>
2022-02-22 21:35:49 +0900

    feat: 홈 페이지('/') 에 ProductListContainer(제품 목록 보여주는) 컴포넌트 추가

---
+++ client/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import ProductListContainer from '../components/ProductList/ProductListContainer';
+
+const Home: NextPage = () => {
+  return <ProductListContainer />;
+};
+
+export default Home;
