4d7391069c11a91a698a51e9a1f80dfff7ab7155
woobottle <dkrnfls@gmail.com>
2022-02-23 13:34:59 +0900

    add jest testing

--- client/src/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.(j|t)s?$": "babel-jest",
-  },
-  transformIgnorePatterns: ["node_modules/(?!(lit-element|lit-html)/)"],
-};
--- client/src/test/Product.test.tsx
+++ client/src/test/Product.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 import ProductListCard from "../components/ProductListCard";
+import { theme } from "../assets/styles/theme";
 
 describe("ProductListCard", () => {
-  it("Product List Card", () => {
+  test("Product List Card", () => {
     const product = {
       id: 1645426258567,
       name: "냉면용기(대)",
@@ -10,7 +13,13 @@ describe("ProductListCard", () => {
       imageUrl: "https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg",
     };
 
-    render(<ProductListCard {...product} />);
+    render(
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <ProductListCard {...product} />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
 
     const productCard = screen.getByText(product.name);
     expect(productCard).not.toBeNull();
