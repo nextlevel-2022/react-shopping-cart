26613a9967f36878cbd7c53e59ae0370c5fe2af0
g1moon <gmoon.cho@gmail.com>
2022-03-01 16:53:18 +0900

    feat: Story에 전역 스타일, 상태 적용(storybook preview.js 수정) - feat: Story에 GlobalStyle 적용 - feat: Story에 Redux store Provider 추가

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,10 +1,22 @@
-// export const parameters = {
-//   actions: {argTypesRegex: "^on[A-Z].*"},
-//   controls: {
-//     matchers: {
-//       color: /(background|color)$/i,
-//       date: /Date$/,
-//     },
-//   },
-// };
-// /
+import { addDecorator } from '@storybook/react';
+
+import { Provider } from 'react-redux';
+import { store } from '../store';
+import GlobalStyle from '../styles/GlobalStyle';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+addDecorator((Story) => (
+  <Provider store={store}>
+    <GlobalStyle />
+    <Story />
+  </Provider>
+));
