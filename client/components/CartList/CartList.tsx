414cae180a853633af030969cd93a93a0a2641b8
g1moon <gmoon.cho@gmail.com>
2022-03-09 19:16:58 +0900

    feat: carts(장바구니 정보)를 props로 받아 아이템들을 뿌려주는 CartList 컴포넌트 추가
      - feat: addSelectedItemToCarts(선택된 아이템을 장바구니로 추가)
    
      - feat: removeSelectedItemFromCarts(선택된 장바구니 아이템을 삭제)
    
      - feat: mutateSelectedCartItems(선택된 장바구니 아이템을 수정, 수량 조절)
    
      - feat: deleteSelectedCartItem(선택된 장바구니 아이템을 삭제)
    
      - feat: clearSelectedCartItems(장바구니 아이템을 모두 삭제)
    
      - feat: selectAllCartItems(모든 장바구니 아이템을 선택)
    
      - feat: orderSelectedCartItem(선택된 장바구니 아이템을 주문)

---
+++ client/components/CartList/CartList.tsx
@@ -0,0 +1,117 @@
+import { useRouter } from 'next/router';
+import { MouseEvent, useState } from 'react';
+
+import useCarts from '../../hooks/service/useCarts';
+import { URL } from '../../shared/constants/url';
+import { CartItem } from '../../shared/types';
+import Spinner from '../Spinner/Spinner';
+import CartListItem from './CartListItem';
+
+interface Props {
+  carts: CartItem[];
+  isLoading: boolean;
+}
+
+const CartList = ({ carts, isLoading }: Props) => {
+  const [selectedCartItems, setSelectedCartItems] = useState<CartItem[]>([]);
+
+  const { deleteCartItemById } = useCarts();
+
+  const router = useRouter();
+
+  const addSelectedItemToCarts = (cartItemToBeAdded: CartItem) => {
+    setSelectedCartItems((prevSelectedCartItems) => [...prevSelectedCartItems, cartItemToBeAdded]);
+  };
+
+  const removeSelectedItemFromCarts = (cartItemToBeRemoved: CartItem) => {
+    setSelectedCartItems((prevSelectedCartItems) =>
+      prevSelectedCartItems.filter((cartItem) => cartItem !== cartItemToBeRemoved),
+    );
+  };
+
+  const mutateSelectedCartItems = ({ target }: MouseEvent<HTMLInputElement>, cartItemBeMutated: CartItem) => {
+    const isSelected = (target as HTMLInputElement).checked;
+
+    switch (true) {
+      case isSelected:
+        addSelectedItemToCarts(cartItemBeMutated);
+        break;
+
+      case !isSelected:
+        removeSelectedItemFromCarts(cartItemBeMutated);
+        break;
+    }
+  };
+
+  const deleteSelectedCartItem = () => {
+    selectedCartItems.forEach(({ id }) => deleteCartItemById(id));
+  };
+
+  const clearSelectedCartItems = () => {
+    setSelectedCartItems([]);
+  };
+
+  const selectAllCartItems = () => {
+    setSelectedCartItems([...carts]);
+  };
+
+  const onClickSelectAllButton = (event: MouseEvent) => {
+    const isActivatedSelectAll = (event.target as HTMLInputElement).checked;
+
+    if (isActivatedSelectAll) selectAllCartItems();
+    else clearSelectedCartItems();
+  };
+
+  const orderSelectedCartItem = () => {
+    const isCartItemSelected = selectedCartItems.length > 0;
+
+    if (!isCartItemSelected) return alert('선택된 아이템이 없습니다.');
+
+    const isPaymentConfirmTrue = confirm('결제 하시겠습니까?');
+
+    if (!isPaymentConfirmTrue) return;
+
+    console.log('--', selectedCartItems);
+
+    deleteSelectedCartItem();
+
+    router.push(URL.ORDER());
+  };
+
+  if (isLoading) return <Spinner>로딩중</Spinner>;
+
+  return (
+    <div>
+      <h1>장바구니</h1>
+      <label htmlFor="selectAllButton">전체선택</label>
+      <input id="selectAllButton" type={'checkbox'} onClick={(event) => onClickSelectAllButton(event)} />
+      <button onClick={deleteSelectedCartItem}>상품삭제</button>
+      <div>
+        <div>든든 배송 상품 ({carts.length}개)</div>
+        <div>결제 예상 금액:{calculateExpectedPrice(selectedCartItems)}</div>
+        <button onClick={orderSelectedCartItem}>주문하기({selectedCartItems.length})</button>
+      </div>
+      <div>
+        {carts.map((cartItem, index) => (
+          <CartListItem
+            key={`cart-item-${index}`}
+            cartItem={cartItem}
+            onClickCartItemSelectButton={mutateSelectedCartItems}
+            selectedCartItems={selectedCartItems}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const calculateExpectedPrice = (selectedCartItems: CartItem[]) => {
+  const PRICE_WHEN_SELECTED_NOTHING = 0;
+
+  return selectedCartItems.reduce(
+    (acc, { product, quantity }) => acc + product.price * quantity,
+    PRICE_WHEN_SELECTED_NOTHING,
+  );
+};
+
+export default CartList;
