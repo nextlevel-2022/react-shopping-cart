464862c1d4e347e734938e7d7b2b8f33bee60e2f
g1moon <gmoon.cho@gmail.com>
2022-03-09 19:37:39 +0900

    refacotr: GetProductsResponseType 타입 타입만 모아두는 types 파일로 이동

--- client/apis/products.ts
+++ client/apis/products.ts
@@ -1,10 +1,6 @@
 import { REQUEST } from '../shared/constants/url';
 import { fetcher } from '../shared/utils/fetcher';
 
-interface GetProductsResponseType {
-  products: any;
-}
-
 const productsService = {
   getProducts: async () => {
     const { method, url } = REQUEST.GET_PRODUCTS();
