cca111a0377e250b6d0afd0b4651f9a4ed5cbdb3
g1moon <gmoon.cho@gmail.com>
2022-03-09 19:36:53 +0900

    feat: cartsService 추가(carts관련 request)   - feat: getCarts(장바구니 목록 받아오기) request 추가
      - feat: deleteCartItemById(장바구니 아이템 지우기) request 추가
    
      - feat: addCartItem(장바구니에 아이템 추가하기) request 추가

---
+++ client/apis/carts.ts
@@ -0,0 +1,25 @@
+import { REQUEST } from '../shared/constants/url';
+import { CartItem, Product } from '../shared/types';
+import { fetcher } from '../shared/utils/fetcher';
+
+const cartsService = {
+  getCarts: async () => {
+    const { method, url } = REQUEST.GET_CARTS();
+
+    return await fetcher(method, url);
+  },
+
+  deleteCartItemById: async (idToDeleteCartItem: CartItem['id']) => {
+    const { method, url } = REQUEST.DELETE_CART_BY_ID(idToDeleteCartItem);
+
+    return await fetcher(method, url);
+  },
+
+  addCartItem: async (product: Product) => {
+    const { method, url } = REQUEST.POST_CART();
+
+    return await fetcher(method, url, { product });
+  },
+};
+
+export default cartsService;
