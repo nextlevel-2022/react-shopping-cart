d7fb70c8ef4c90aa3cceab3aa4d3afa4e4a03937
g1moon <gmoon.cho@gmail.com>
2022-03-09 19:38:35 +0900

    feat: productsService에 getProductById request 추가   - feat: getProductById(아이디 값을 이용해 장바구니 아이템을 가져오는) 기능 추가

--- client/apis/products.ts
+++ client/apis/products.ts
@@ -1,9 +1,16 @@
 import { REQUEST } from '../shared/constants/url';
+import { Product } from '../shared/types';
 import { fetcher } from '../shared/utils/fetcher';
 
 const productsService = {
   getProducts: async () => {
     const { method, url } = REQUEST.GET_PRODUCTS();
+
+    return await fetcher(method, url);
+  },
+
+  getProductById: async (productId: Product['id']) => {
+    const { method, url } = REQUEST.GET_PRODUCT_BY_ID(productId);
 
     return await fetcher(method, url);
   },
