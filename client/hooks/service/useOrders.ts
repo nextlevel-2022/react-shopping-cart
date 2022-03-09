ca680cead05d4b5e56cd58a421eba914a16e99a5
g1moon <gmoon.cho@gmail.com>
2022-03-09 19:35:12 +0900

    feat: orders 스토어 관련 redux-service 관리하는 커스텀 훅 추가
    - feat: orders 상태 셀렉터 추가(orders,isLoadingOrders, hasErrorOrders, errorOrders)
    
    - feat: getOrders(dispatch getOrders.request) orders 데이터 받아오는 액션 디스패치

---
+++ client/hooks/service/useOrders.ts
@@ -0,0 +1,40 @@
+import { createSelector } from 'reselect';
+
+import { BaseRequestReducerState, RootState } from '../../shared/types';
+import { createModelAttributeObject, createModelAttributeSelector } from '../../shared/utils/redux';
+import { useAppDispatch } from '../../store';
+import { ordersActions } from '../../store/modules/orders/slice';
+import { OrderItemFromServer, ORDERS } from '../../store/modules/orders/types';
+
+const useOrders = () => {
+  const dispatch = useAppDispatch();
+
+  const selfSelector = (state: RootState) => state[ORDERS];
+  const ordersModelSelector = createSelector(
+    selfSelector,
+    (state): BaseRequestReducerState<OrderItemFromServer[]> => state.orders,
+  );
+
+  const ordersAttributeSelector = createModelAttributeSelector(ordersModelSelector);
+
+  const {
+    value: orders,
+    isLoading: isLoadingOrders,
+    hasError: hasErrorOrders,
+    error: errorOrders,
+  } = createModelAttributeObject<OrderItemFromServer[]>(ordersAttributeSelector);
+
+  const getOrders = () => {
+    dispatch(ordersActions.getOrders.request());
+  };
+
+  return {
+    orders,
+    isLoadingOrders,
+    hasErrorOrders,
+    errorOrders,
+    getOrders,
+  };
+};
+
+export default useOrders;
