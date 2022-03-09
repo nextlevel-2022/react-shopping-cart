5d90481c4333ea55655fd0cdbc534cad0f078643
g1moon <gmoon.cho@gmail.com>
2022-03-09 18:44:03 +0900

    feat: carts 스토어 관련 타입 추가   - CARTS: 'carts' 상수
      - GetCartsSuccessPayload: getCarts 성공 시 payload
      DeleteCartItemRequestPayload: deleteCartItem 요청 시 paylaod
    
      - DeleteCartItemSuccessPayload: deleteCartItem 성공 시 payload
    
      - CartsReducerState: cartsReducer 의 상태 타입
    
      - AddCartItemRequestPayload: addCartItem 요청 시 payload
    
      - AddCartItemSuccessPayload: addCartItem 성공 시 paylaod

---
+++ client/store/modules/carts/types.ts
@@ -0,0 +1,28 @@
+import { BaseRequestReducerState, CartItem, Product } from '../../../shared/types';
+
+export const CARTS = 'carts';
+
+export interface GetCartsSuccessPayload {
+  carts: Omit<CartItem, 'quantity'>[];
+}
+
+export interface DeleteCartItemRequestPayload {
+  idToDeleteCartItem: CartItem['id'];
+}
+
+export interface DeleteCartItemSuccessPayload {
+  deletedCartItemId: CartItem['id'];
+}
+
+export interface CartsReducerState {
+  selectedProducts: Product[];
+  carts: BaseRequestReducerState<CartItem[]>;
+}
+
+export interface AddCartItemRequestPayload {
+  product: Product;
+}
+
+export interface AddCartItemSuccessPayload {
+  newCartItemProduct: Product;
+}
