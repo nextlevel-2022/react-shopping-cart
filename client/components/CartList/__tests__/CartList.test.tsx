ad11cc0d3bd1be5867005e64fd5437fcef943e59
g1moon <gmoon.cho@gmail.com>
2022-03-15 21:17:37 +0900

    chore: CartItem 타입 변경으로 인한 id 프로퍼티 추가

--- client/components/CartList/__tests__/CartList.test.tsx
+++ client/components/CartList/__tests__/CartList.test.tsx
@@ -9,6 +9,7 @@ import CartList from '../CartList';
 const cartItem: CartItem = {
   id: 1,
   product: {
+    id: 1,
     name: '[리뉴얼]젓가락(종이)-정성을 담아',
     price: 21800,
     imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/5297837f-5ecd-4945-be2f-4a75854cd06e.jpg',
