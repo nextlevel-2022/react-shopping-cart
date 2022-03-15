f0987f87d929a55578c7aefd7943248e883d9096
g1moon <gmoon.cho@gmail.com>
2022-03-15 21:21:30 +0900

    test: OrderList(주문 목록) 테스트 추가   - feat: renderOrderList 테스트 코드 간소화를 위한 렌더 함수 추가
      - describe('<OrderList />'
          - it('로딩중인 경우 로딩임을 보여준다.')
    
          - it('로딩중이 아닌 경우 총 주문 상품 개수와 액수를 보여준다.')
    
          - it('주문내역(orders)에 아무것도 들어있지 않다면 주문 내역이 존재하지 않다는 메시지를 보여준다.')

---
+++ client/components/Orders/__tests__/OrderList.test.tsx
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react';
+
+import { getTotalOrderAmount, getTotalOrderItemNumber } from '../../../service/ordersService';
+import { orders } from '../../../shared/fixtures/orders';
+import OrderList, { Props } from '../OrderList';
+
+describe('<OrderList />', () => {
+  it('로딩중인 경우 로딩임을 보여준다.', () => {
+    const { LoadingText } = renderOrderList({
+      orders,
+      isLoading: true,
+    });
+
+    expect(LoadingText()).toBeInTheDocument();
+  });
+
+  it('로딩중이 아닌 경우 총 주문 상품 개수와 액수를 보여준다.', () => {
+    const { TotalOrderItemNumberText, TotalOrderAmountText } = renderOrderList({
+      orders,
+      isLoading: false,
+    });
+
+    expect(TotalOrderItemNumberText()).toBeInTheDocument();
+    expect(TotalOrderAmountText()).toBeInTheDocument();
+  });
+
+  it('주문내역(orders)에 아무것도 들어있지 않다면 주문 내역이 존재하지 않다는 메시지를 보여준다.', () => {
+    const { NoneOrderItemText } = renderOrderList({ orders: [], isLoading: false });
+
+    expect(NoneOrderItemText()).toBeInTheDocument();
+  });
+});
+
+/** render utils */
+const renderOrderList = ({ orders, isLoading }: Props) => {
+  const result = render(<OrderList orders={orders} isLoading={isLoading} />);
+
+  const LoadingText = () => result.getByText('로딩중');
+  const TotalOrderItemNumberText = () => result.getByText(`총 주문 상품 개수: ${getTotalOrderItemNumber(orders)}`);
+  const TotalOrderAmountText = () => result.getByText(`총 주문 액수: ${getTotalOrderAmount(orders)}`);
+
+  const NoneOrderItemText = () => result.getByText('주문 내역이 존재하지 않습니다.');
+
+  return { result, LoadingText, TotalOrderItemNumberText, TotalOrderAmountText, NoneOrderItemText };
+};
