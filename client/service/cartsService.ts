5ae4acc9ee59cbeee68e78b24c135eae88db8d64
g1moon <gmoon.cho@gmail.com>
2022-03-17 01:05:41 +0900

    refactor: cartsService 관련 함수들 리턴 타입 추가   - refactor: isMaxCartItemQuantity 리턴 타입 void -> boolean 으로 변경
      - refactor: isMinCartItemQuantity 리턴 타입 void -> boolean 으로 변경
    
      - refactor: hasSameProductInCarts 리턴 타입 void -> boolean 으로 변경

--- client/service/cartsService.ts
+++ client/service/cartsService.ts
@@ -1,18 +1,18 @@
 import { CartItem, OrderDetail, Product } from '../shared/types';
 
-export const isMaxCartItemQuantity = (currentQuantity: CartItem['quantity']) => {
+export const isMaxCartItemQuantity = (currentQuantity: CartItem['quantity']): boolean => {
   const MAX_CART_ITEM_QUANTITY = 20;
 
   return MAX_CART_ITEM_QUANTITY === currentQuantity;
 };
 
-export const isMinCartItemQuantity = (currentQuantity: CartItem['quantity']) => {
+export const isMinCartItemQuantity = (currentQuantity: CartItem['quantity']): boolean => {
   const MIN_CART_ITEM_QUANTITY = 1;
 
   return MIN_CART_ITEM_QUANTITY === currentQuantity;
 };
 
-export const hasSameProductInCarts = (carts: CartItem[], newProduct: Product) => {
+export const hasSameProductInCarts = (carts: CartItem[], newProduct: Product): boolean => {
   return carts.findIndex(({ product }) => product.id === newProduct.id) >= 0;
 };
 
