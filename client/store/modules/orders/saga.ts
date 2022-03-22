0da732da3081a428070e1c5768e59966cca33be7
g1moon <gmoon.cho@gmail.com>
2022-03-22 23:50:17 +0900

    feat: postOrdersSaga(주문하기 관련 기능) 추가

--- client/store/modules/orders/saga.ts
+++ client/store/modules/orders/saga.ts
@@ -1,7 +1,9 @@
-import { call, put, takeLatest } from '@redux-saga/core/effects';
+import { call, put, takeEvery, takeLatest } from '@redux-saga/core/effects';
+import { PayloadAction } from '@reduxjs/toolkit';
 
-import ordersRequest from '../../../apis/orders';
+import ordersRequest from '../../../service/apis/orders';
 import { OrderItem } from '../../../shared/types';
+import { PostOrdersSuccessPayload } from '../carts/types';
 import { ordersActions } from './slice';
 
 function* getOrdersSaga() {
@@ -16,8 +18,21 @@ function* getOrdersSaga() {
   }
 }
 
+function* postOrdersSaga({ payload: { newOrderDetails } }: PayloadAction<PostOrdersSuccessPayload>) {
+  try {
+    yield call(ordersRequest.addOrderItem, newOrderDetails);
+
+    yield put(ordersActions.postOrders.success({ newOrderDetails }));
+  } catch (error) {
+    if (error instanceof Error) {
+      yield put(ordersActions.postOrders.failure({ error }));
+    }
+  }
+}
+
 export default function* ordersSaga() {
-  const { getOrders } = ordersActions;
+  const { getOrders, postOrders } = ordersActions;
 
   yield takeLatest(getOrders.request, getOrdersSaga);
+  yield takeEvery(postOrders.request, postOrdersSaga);
 }
