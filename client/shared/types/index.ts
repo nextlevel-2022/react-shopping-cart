ef15be2fe1c65df1dc39b7c634e25a99ebe860c2
g1moon <gmoon.cho@gmail.com>
2022-03-22 23:44:00 +0900

    feat: ButtonSize(Button 컴포넌트의 버튼 사이즈에 대한 props) 타입 추가

--- client/shared/types/index.ts
+++ client/shared/types/index.ts
@@ -3,6 +3,7 @@ import { COLOR } from '../constants/css';
 import { REQUEST } from '../constants/url';
 
 export type Color = typeof COLOR[keyof typeof COLOR];
+export type ButtonSize = typeof BUTTON_SIZE[keyof typeof BUTTON_SIZE];
 
 export type RequestMethod = 'get' | 'post' | 'delete' | 'put';
 export type RequestInfo = { method: RequestMethod; url: string };
